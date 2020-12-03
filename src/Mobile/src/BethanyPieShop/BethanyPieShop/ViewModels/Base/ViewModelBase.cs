30092221109fed8538bb9737148170e92ad68429
s v <chofexx@gmail.com>
2020-12-03 17:16:38 +0200

    Adding ViewModel Base

---
+++ src/Mobile/src/BethanyPieShop/BethanyPieShop/ViewModels/Base/ViewModelBase.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+namespace BethanyPieShop.Core.ViewModels.Base
+{
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        private bool _isBusy;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public bool IsBusy
+        {
+            get => _isBusy;
+            set
+            {
+                _isBusy = value;
+                OnPropertyChanged(nameof(IsBusy));
+            }
+        }
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public virtual Task InitializeAsync(object data)
+        {
+            return Task.FromResult(false);
+        }
+    }
+}
