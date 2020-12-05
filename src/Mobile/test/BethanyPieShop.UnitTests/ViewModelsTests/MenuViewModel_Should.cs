687061d406ac70beb3b60e29a73126078009ed62
s v <chofexx@gmail.com>
2020-12-05 07:22:42 +0200

    Adding MenuViewModel MenuItemTappedCommand Test

---
+++ src/Mobile/test/BethanyPieShop.UnitTests/ViewModelsTests/MenuViewModel_Should.cs
@@ -0,0 +1,27 @@
+﻿using BethanyPieShop.Core.Contracts;
+using BethanyPieShop.Core.ViewModels;
+using Moq;
+using NUnit.Framework;
+
+namespace BethanyPieShop.UnitTests.ViewModelsTests
+{
+    [TestFixture]
+    public class MenuViewModel_Should
+    {
+        [Test]
+        public void Not_Return_Null_MenuItemTappedCommand()
+        {
+
+            var settingsMock = new Mock<ISettingsService>();
+            var navigationMock = new Mock<INavigationService>();
+
+            var menuWiewModel = new MenuViewModel(
+                navigationMock.Object,
+                settingsMock.Object);
+
+            var loginCommand = menuWiewModel.MenuItemTappedCommand;
+
+            Assert.NotNull(loginCommand);
+        }
+    }
+}
