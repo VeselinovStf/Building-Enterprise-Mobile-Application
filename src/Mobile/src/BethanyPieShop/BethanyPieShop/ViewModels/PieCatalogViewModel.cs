eed8bf0b5dc317885882c6c001c7911ab82289cb
s v <chofexx@gmail.com>
2020-12-05 09:27:14 +0200

    Adding PieCatalogViewModel

---
+++ src/Mobile/src/BethanyPieShop/BethanyPieShop/ViewModels/PieCatalogViewModel.cs
@@ -0,0 +1,88 @@
+﻿using BethanyPieShop.Core.Contracts;
+using BethanyPieShop.Core.Extensions;
+using BethanyPieShop.Core.Models;
+using BethanyPieShop.Core.ViewModels.Base;
+using System;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace BethanyPieShop.Core.ViewModels
+{
+    public class PieCatalogViewModel : ViewModelBase
+    {
+        private readonly IConnectionService _connectionService;
+        private readonly INavigationService _navigationService;
+        private readonly IDialogService _dialogService;
+        private readonly ICatalogDataService _catalogDataService;
+
+        private ObservableCollection<Pie> _pies;
+
+        public PieCatalogViewModel(
+            IConnectionService connectionService,
+            INavigationService navigationService,
+            IDialogService dialogService,
+            ICatalogDataService catalogDataService)
+        {
+            _connectionService = connectionService;
+            _navigationService = navigationService;
+            _dialogService = dialogService;
+            _catalogDataService = catalogDataService;
+
+            Pies = new ObservableCollection<Pie>();
+        }
+
+        public ICommand PieTappedCommand => new Command<Pie>(OnPieTapped);
+
+        public ObservableCollection<Pie> Pies
+        {
+            get => _pies;
+            set
+            {
+                _pies = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private void OnPieTapped(Pie selectedPie)
+        {
+            _navigationService.NavigateToAsync<PieDetailViewModel>(selectedPie);
+        }
+
+      
+        public override async Task InitializeAsync(object data)
+        {
+            IsBusy = true;
+
+            if (_connectionService.IsConnected)
+            {
+                try
+                {
+                    var piesOfTheWeakServiceCall = await _catalogDataService.GetAllPiesAsync();
+
+                    Pies = piesOfTheWeakServiceCall.ToObservableCollection();
+                }
+                catch (Exception ex)
+                {
+                    //TODO: Exception logging
+                    await _dialogService.ShowDialog(
+                        "Unexpected error accures, please try again later",
+                        "Try Later",
+                        "Ok"
+                        );
+                }
+            }
+            else
+            {
+                await _dialogService.ShowDialog(
+                        "Make shour you are connectet to internet",
+                        "No Internet Connection",
+                        "Ok"
+                        );
+            }
+
+            IsBusy = false;
+        }
+    }
+}
---
+++ src/Mobile/test/BethanyPieShop.UnitTests/ViewModelsTests/PieCatalogViewModel_Should.cs
@@ -0,0 +1,53 @@
+﻿using BethanyPieShop.Core.Contracts;
+using BethanyPieShop.Core.ViewModels;
+using Moq;
+using NUnit.Framework;
+
+namespace BethanyPieShop.UnitTests.ViewModelsTests
+{
+    [TestFixture]
+    public class PieCatalogViewModel_Should
+    {
+        [Test]
+        public void Return_Not_Null_PieTappedCommand()
+        {
+            var connectivityMock = new Mock<IConnectionService>();
+            var dialogogMock = new Mock<IDialogService>();
+            var navigationMock = new Mock<INavigationService>();
+            var catalogDataServiceMock = new Mock<ICatalogDataService>();
+
+            var pieDetailViewModel = new PieCatalogViewModel(
+                connectivityMock.Object,
+                navigationMock.Object,
+                dialogogMock.Object,
+                catalogDataServiceMock.Object
+                );
+
+            var pieTappedCommand = pieDetailViewModel.PieTappedCommand;
+
+            Assert.NotNull(pieTappedCommand);
+        }
+
+
+        [Test]
+        public void Return_Not_Null_Pies()
+        {
+            var connectivityMock = new Mock<IConnectionService>();
+            var dialogogMock = new Mock<IDialogService>();
+            var navigationMock = new Mock<INavigationService>();
+            var catalogDataServiceMock = new Mock<ICatalogDataService>();
+
+            var pieDetailViewModel = new PieCatalogViewModel(
+                connectivityMock.Object,
+                navigationMock.Object,
+                dialogogMock.Object,
+                catalogDataServiceMock.Object
+                );
+
+            var pies = pieDetailViewModel.Pies;
+
+            Assert.NotNull(pies);
+        }
+
+    }
+}
