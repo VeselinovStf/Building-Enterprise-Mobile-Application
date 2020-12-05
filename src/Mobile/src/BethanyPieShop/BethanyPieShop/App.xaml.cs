9f39a8842b4cf0fb3a8999c10b2e437f10cafcef
s v <chofexx@gmail.com>
2020-12-05 11:15:24 +0200

    Adding AppContainer DI

--- src/Mobile/src/BethanyPieShop/BethanyPieShop/App.xaml.cs
+++ src/Mobile/src/BethanyPieShop/BethanyPieShop/App.xaml.cs
@@ -1,4 +1,8 @@
-﻿using Xamarin.Forms;
+﻿using BethanyPieShop.Core.Bootstrap;
+using BethanyPieShop.Core.Contracts;
+using BethanyPieShop.Core.ViewModels;
+using System.Threading.Tasks;
+using Xamarin.Forms;
 
 namespace BethanyPieShop.Core
 {
@@ -8,19 +12,38 @@ namespace BethanyPieShop.Core
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            InitializeApp();
+
+            InitializeNavigation();
+        }
+
+        private async Task InitializeNavigation()
+        {
+            var navigationService = AppContainer.Resolve<INavigationService>();
+            await navigationService.InitializeAsync();
+        }
+
+        private void InitializeApp()
+        {
+            AppContainer.RegisterDependencies();
+
+            var shoppingCartViewModel = AppContainer.Resolve<ShoppingCartViewModel>();
+            shoppingCartViewModel.InitializeMessenger();
         }
 
         protected override void OnStart()
         {
+            // Handle when your app starts
         }
 
         protected override void OnSleep()
         {
+            // Handle when your app sleeps
         }
 
         protected override void OnResume()
         {
+            // Handle when your app resumes
         }
     }
 }
---
+++ src/Mobile/src/BethanyPieShop/BethanyPieShop/Bootstrap/AppContainer.cs
@@ -0,0 +1,62 @@
+﻿using Autofac;
+using BethanyPieShop.Core.Contracts;
+using BethanyPieShop.Core.Services.Data;
+using BethanyPieShop.Core.Services.General;
+using BethanyPieShop.Core.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BethanyPieShop.Core.Bootstrap
+{
+    public class AppContainer
+    {
+        private static IContainer _container;
+
+        public static void RegisterDependencies()
+        {
+            var builder = new ContainerBuilder();
+
+            //ViewModels
+            builder.RegisterType<CheckoutViewModel>();
+            builder.RegisterType<ContactViewModel>();
+            builder.RegisterType<LoginViewModel>();
+            builder.RegisterType<MainViewModel>();
+            builder.RegisterType<PieCatalogViewModel>();
+            builder.RegisterType<PieDetailViewModel>();
+            builder.RegisterType<RegistrationViewModel>();
+            builder.RegisterType<ShoppingCartViewModel>().SingleInstance();
+            builder.RegisterType<MenuViewModel>();
+            builder.RegisterType<HomeViewModel>();
+
+            //services - data
+            builder.RegisterType<CatalogDataService>().As<ICatalogDataService>();
+            builder.RegisterType<ShoppingCartDataService>().As<IShoppingCartDataService>();
+            builder.RegisterType<ContactDataService>().As<IContactDataService>();
+            builder.RegisterType<OrderDataService>().As<IOrderDataService>();
+
+            //services - general
+            builder.RegisterType<ConnectionService>().As<IConnectionService>();
+            builder.RegisterType<NavigationService>().As<INavigationService>();
+            builder.RegisterType<AuthenticationService>().As<IAuthenticationService>();
+            builder.RegisterType<DialogService>().As<IDialogService>();
+            builder.RegisterType<PhoneContactService>().As<IPhoneContactService>();
+            builder.RegisterType<SettingsService>().As<ISettingsService>().SingleInstance();
+
+            //General
+            builder.RegisterType<RequestProvider>().As<IRequestProvider>();
+
+            _container = builder.Build();
+        }
+
+        public static object Resolve(Type typeName)
+        {
+            return _container.Resolve(typeName);
+        }
+
+        public static T Resolve<T>()
+        {
+            return _container.Resolve<T>();
+        }
+    }
+}
